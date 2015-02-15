44acd4bb81c5305c6d7e548e62d6ab3d59cdbdf9
emanb29 <ebgamer29@gmail.com>
2015-02-14 16:22:14 -0800

    Adding templates and registration form

--- www/js/app.js
+++ www/js/app.js
@@ -4,31 +4,183 @@
 
 
 /*jslint browser:true, devel:true, white:true, vars:true */
-/*global $:false, intel:false app:false, dev:false, cordova:false */
+/*global $:false, intel:false, app:false, dev:false */
+/*global myEventHandler:false, cordova:false, device:false */
 
 
 
-// This file contains your event handlers, the center of your application.
-// NOTE: see app.initEvents() in init-app.js for event handler initialization code.
+window.app = window.app || {} ;         // there should only be one of these...
 
-function myEventHandler() {
+
+
+// Set to "true" if you want the console.log messages to appear.
+
+app.LOG = false ;
+
+app.consoleLog = function() {           // only emits console.log messages if app.LOG != false
+    if( app.LOG ) {
+        var args = Array.prototype.slice.call(arguments, 0) ;
+        console.log.apply(console, args) ;
+    }
+} ;
+
+
+
+// App init point (runs on custom app.Ready event from init-dev.js).
+// Runs after underlying device native code and webview/browser is ready.
+// Where you should "kick off" your application by initializing app events, etc.
+
+// NOTE: Customize this function to initialize your application, as needed.
+
+app.initEvents = function() {
     "use strict" ;
+    var fName = "app.initEvents():" ;
+    app.consoleLog(fName, "entry") ;
 
-    var ua = navigator.userAgent ;
-    var str ;
+    // NOTE: initialize your third-party libraries and event handlers
 
-    if( window.Cordova && dev.isDeviceReady.c_cordova_ready__ ) {
-            str = "It worked! Cordova device ready detected at " + dev.isDeviceReady.c_cordova_ready__ + " milliseconds!" ;
+    // initThirdPartyLibraryNumberOne() ;
+    // initThirdPartyLibraryNumberTwo() ;
+    // initThirdPartyLibraryNumberEtc() ;
+
+    // NOTE: initialize your application code
+
+    // initMyAppCodeNumberOne() ;
+    // initMyAppCodeNumberTwo() ;
+    // initMyAppCodeNumberEtc() ;
+
+    // NOTE: initialize your app event handlers, see app.js for a simple event handler example
+
+    // TODO: configure following to work with both touch and click events (mouse + touch)
+    // see http://msopentech.com/blog/2013/09/16/add-pinch-pointer-events-apache-cordova-phonegap-app/
+
+    var el, evt ;
+
+    if( navigator.msPointerEnabled || !('ontouchend' in window))    // if on Win 8 machine or no touch
+        evt = "click" ;                                             // let touch become a click event
+    else                                                            // else, assume touch events available
+        evt = "touchend" ;                                          // not optimum, but works
+
+
+
+    // NOTE: ...you can put other miscellaneous init stuff in this function...
+    // NOTE: ...and add whatever else you want to do now that the app has started...
+
+    app.initDebug() ;           // just for debug, not required; keep it if you want it or get rid of it
+    app.hideSplashScreen() ;    // after init is good time to remove splash screen; using a splash screen is optional
+
+    // app initialization is done
+    // app event handlers are ready
+    // exit to idle state and wait for app events...
+
+    app.consoleLog(fName, "exit") ;
+} ;
+document.addEventListener("app.Ready", app.initEvents, false) ;
+
+
+
+// Just a bunch of useful debug console.log() messages.
+// Runs after underlying device native code and webview/browser is ready.
+// The following is just for debug, not required; keep it if you want or get rid of it.
+
+app.initDebug = function() {
+    "use strict" ;
+    var fName = "app.initDebug():" ;
+    app.consoleLog(fName, "entry") ;
+
+    if( window.device && device.cordova ) {                     // old Cordova 2.x version detection
+        app.consoleLog("device.version: " + device.cordova) ;   // print the cordova version string...
+        app.consoleLog("device.model: " + device.model) ;
+        app.consoleLog("device.platform: " + device.platform) ;
+        app.consoleLog("device.version: " + device.version) ;
     }
-    else if( window.intel && intel.xdk && dev.isDeviceReady.d_xdk_ready______ ) {
-            str = "It worked! Intel XDK device ready detected at " + dev.isDeviceReady.d_xdk_ready______ + " milliseconds!" ;
+
+    if( window.cordova && cordova.version ) {                   // only works in Cordova 3.x
+        app.consoleLog("cordova.version: " + cordova.version) ; // print new Cordova 3.x version string...
+
+        if( cordova.require ) {                                 // print included cordova plugins
+            app.consoleLog(JSON.stringify(cordova.require('cordova/plugin_list').metadata, null, 1)) ;
+        }
     }
-    else {
-        str = "Bad device ready, or none available because we're running in a browser." ;
+
+    app.consoleLog(fName, "exit") ;
+} ;
+
+
+
+// Using a splash screen is optional. This function will not fail if none is present.
+// This is also a simple study in the art of multi-platform device API detection.
+
+app.hideSplashScreen = function() {
+    "use strict" ;
+    var fName = "app.hideSplashScreen():" ;
+    app.consoleLog(fName, "entry") ;
+
+    // see https://github.com/01org/appframework/blob/master/documentation/detail/%24.ui.launch.md
+    // Do the following if you disabled App Framework autolaunch (in index.html, for example)
+    // $.ui.launch() ;
+
+    if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
+        navigator.splashscreen.hide() ;
+    }
+    if( window.intel && intel.xdk && intel.xdk.device ) {           // Intel XDK device API detected, but...
+        if( intel.xdk.device.hideSplashScreen )                     // ...hideSplashScreen() is inside the base plugin
+            intel.xdk.device.hideSplashScreen() ;
     }
 
-    alert(str) ;
-}
+    app.consoleLog(fName, "exit") ;
+} ;
 
+angular.module('socialshopping', ['ngRoute', 'ui.bootstrap']);
+angular.module('socialshopping')
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+      $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'indexCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'loginCtrl'
+        })
+        .when('/register', {
+            templateUrl: 'views/register.html',
+            controller: 'registerCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+  }])
+  .controller('navCtrl', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location){
+      $scope.isActive = function(page) {
+          var current = $location.path().substring(1);
+          //console.log('current: ', current, ' page: ', page, ' location: ', $location.path());
+          return (page === current || page === $location.path()) ? "active" : "";
+      };
+  }])
+  .controller('indexCtrl', ['$scope', '$rootScope', function($scope, $rootScope){
 
-// ...additional event handlers here...
+  }])
+  .controller('loginCtrl', ['$scope', '$rootScope', function($scope, $rootScope){
+
+  }])
+  .controller('registerCtrl', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location){
+
+    document.scope = $rootScope;
+    $scope.form = {};
+    $scope.registerUser = function() {
+      var user = new Parse.User();
+      user.set('username', $scope.form.username);
+      user.set('password', $scope.form.password);
+      user.set('email', $scope.form.email);
+      user.signUp(null, {
+        success: function(user) {
+          $rootScope.user = $scope.form;
+          $scope.$apply(function() { $location.path("/route"); });
+        },
+        error: function(error) {
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    }
+  }]);
